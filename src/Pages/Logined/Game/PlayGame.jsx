8541816979166d1dabc68d0dfbc17d93ca3213f2
Duke-Saurabh <ds8123495@example.com>
2024-06-27 07:45:19 +0530

    added some major change including suffling card choose and many others

--- src/Pages/Logined/Game/PlayGame.jsx
+++ src/Pages/Logined/Game/PlayGame.jsx
@@ -20,6 +20,14 @@ function PlayGame() {
 
     const [importantNotes,setImportantNotes]=useState('Start Game');
 
+    const [cards,setCards]=useState([
+        {role:'Raja',score:1000},{role:'Mantri',score:800},{role:'Sipahi',score:500},{role:'Chor',score:0}
+    ])
+
+    const [scores,setScores]=useState({});
+    const [playerIdx,setPlayerIdx]=useState(0);
+    const [isSuffled,setIsSuffled]=useState(false);
+
     useEffect(() => {
       const fetchTeamMates = async () => {
         const dataObj = {
@@ -174,6 +182,51 @@ function PlayGame() {
         setOpenChat(false);
     }
 
+    //..................................................//
+
+    useEffect(()=>{
+      if(playersJoined.length!=4 ){
+        setImportantNotes(`Waiting for Others to join.`)
+        return;
+      }
+
+      if(isSuffled==false){
+        setImportantNotes(`${playersJoined?.[0]?.name} Press the Card Suffle Button`)
+        return;
+      }
+
+      if(playerIdx==0){
+        setImportantNotes(`${playersJoined?.[0]?.name} Choose a Card`)
+        return;
+      }
+
+      if(playerIdx==1){
+        setImportantNotes(`${playersJoined?.[1]?.name} Choose a Card`)
+        return;
+      }
+
+      if(playerIdx==2){
+        setImportantNotes(`${playersJoined?.[2]?.name} Choose a Card`)
+        return;
+      }
+
+      if(playerIdx==3){
+        setImportantNotes(`${playersJoined?.[3]?.name} Choose a Card`)
+        return;
+      }
+
+      if(playerIdx==4){
+        setImportantNotes(`Raja: "Raja Raj Karta Hai Sipahi Chor Ko Pakdo".`);
+
+        
+      }
+
+      
+
+
+      return;
+    },[playersJoined,playerIdx,isSuffled]);
+
    
     // Style object for the chat area
     const mystYle = {
@@ -181,27 +234,120 @@ function PlayGame() {
         width: '320px', // Fixed width for the chat area (can be adjusted)
     //    height:'auto'
     };
-
-    const mystYleOpenChatBtn={
-        // display:
+    
+    console.log('cards:',cards);
+    const handleCardSuffle=()=>{
+      const cardDetails=[...cards];
+      for(let i=0;i<4;i++){
+        const j=Math.floor(Math.random()*(4));
+        [cardDetails[i],cardDetails[j]]=[cardDetails[j],cardDetails[i]];
+      }
+      socket.emit('/suffle',{team:teamSelected,card:[...cardDetails],suffle:true})
+      console.log('cards:',cards);
     }
 
+    const cardRefs = [useRef(null), useRef(null), useRef(null), useRef(null)];
+    const handleCardClick = (index) => {
+      console.log('card clicked: ', index);
+
+      if (playersJoined?.[playerIdx]?.name !== user?.userName || isSuffled === false) {
+        return;
+      }
+    
+      const { role, score } = cards[index];
+      console.log('role:', role);
+      console.log('score:', score);
+    
+      let newScore, newRole;
+    
+      switch (role) {
+        case 'Raja':
+          newScore = score;
+          newRole = role;
+          break;
+        case 'Sipahi':
+          newScore = 'Wait till End';
+          newRole = role;
+          break;
+        case 'Mantri':
+        case 'Chor':
+          newScore = 'Hidden';
+          newRole = 'Hidden';
+          break;
+        default:
+          return;
+      }
+    
+      // setScores((prev) => ({
+      //   ...prev,
+      //   [playersJoined[playerIdx].name]: { score: newScore, role: newRole },
+      // }));
+    
+      for(let i=0;i<4;i++){
+        cardRefs[i].current.style.display = 'none';
+      }
+      socket.emit(`/cardSelect`, { index, playerIndex:playerIdx, team: teamSelected, score:{...scores,
+        [playersJoined[playerIdx].name]: { score: newScore, role: newRole },
+      }});
+
+      return () => {
+        socket.off(`/${teamSelected}/playerIdx`, handlePlayerIdxUpdate);
+      };
+    };
+
+
+    const handleCardSelectUpdate = ({playerIndex,index,score}) => {
+      cardRefs[index].current.style.display = 'none';
+      setPlayerIdx(playerIndex+1);
+      setScores({...score});
+    };
+
+    const handleCardShuffleUpdate = ({ card, suffle }) => {
+      for (let i = 0; i < 4; i++) {
+        if (cardRefs[i]?.current?.style) {
+          cardRefs[i].current.style.display = 'flex';
+        }
+      }
+      setCards([...card]);
+      console.log(card);
+      setIsSuffled(suffle);
+    };    
+
+
+    useEffect(()=>{
+
+      socket.on(`/${teamSelected}/cardSelect`, handleCardSelectUpdate);
+      socket.on(`/${teamSelected}/suffle`,handleCardShuffleUpdate)
+    
+      return () => {
+        socket.off(`/${teamSelected}/cardSelect`, handleCardSelectUpdate);
+        socket.on(`/${teamSelected}/suffle`,handleCardShuffleUpdate)
+      };
+
+    })
+
+
+    
+
+    console.log('check:', (!isSuffled && playersJoined.length===4 && playersJoined?.[0].name===user.userName) )
     
     return (
         <div className='container'>
             <div className='welcome-note'><h2>Welcome {user?.userName || ''} to Raja Mantri Chor Sipahi</h2></div>
-            <div style={mystYleOpenChatBtn} className='open-chat' onClick={() => setOpenChat(prev => !prev)}>Chat</div>
+            <div className='open-chat' onClick={() => setOpenChat(prev => !prev)}>Chat</div>
             <div className='game-area' onClick={handleClickOutSideChatArea} >
                 {/* Game area content */}
                 <div className='importantNotes'>{importantNotes}</div>
                 <div className='game-show-area'>
                     <div className='player one'>
                         <p className='player-name'>{teamMates?.firstPlayerUserName || 'Player 1'}</p>
-                        <p className='player-score'>Score: 10</p>
+                        <p className='player-role'>Role: {scores[teamMates?.firstPlayerUserName]?.role || 'none'}</p>
+                        <p className='player-score'>Score: {scores[teamMates?.firstPlayerUserName]?.score || 'none'}</p>
                     </div>
                     <div className='player two'>
                         <p className='player-name'>{teamMates?.secondPlayerUserName || 'Player 2'}</p>
-                        <p className='player-score'>Score: 8</p>
+                        <p className='player-role'>Role: {scores[teamMates?.secondPlayerUserName]?.role || 'none'}</p>
+                        <p className='player-score'>Score: {scores[teamMates?.secondPlayerUserName]?.score || 'none'}</p>
                     </div>
                     <div className='score'>
                             <p>Team : {teamSelected}</p>
@@ -223,20 +369,31 @@ function PlayGame() {
 
                     <div className='player three'>
                         <p className='player-name'>{teamMates?.thirdPlayerUserName || 'Player 3'}</p>
-                        <p className='player-score'>Score: 15</p>
+                        <p className='player-role'>Role: {scores[teamMates?.thirdPlayerUserName]?.role || 'none'}</p>
+                        <p className='player-score'>Score: {scores[teamMates?.thirdPlayerUserName]?.score || 'none'}</p>
                     </div>
                     <div className='player four'>
                         <p className='player-name'>{teamMates?.fourthPlayerUserName || 'Player 4'}</p>
-                        <p className='player-score'>Score: 5</p>
+                        <p className='player-role'>Role: {scores[teamMates?.fourthPlayerUserName]?.role || 'none'}</p>
+                        <p className='player-score'>Score: {scores[teamMates?.fourthPlayerUserName]?.score || 'none'}</p>
                     </div>
                 </div>
-                <button className='sufflebtn'>Card Suffle</button>
-                <div className='divider'></div>
+                {
+                  !isSuffled && playersJoined.length===4 && playersJoined?.[0].name===user.userName &&
+                   (
+                   <button className='shufflebtn' type='button' onClick={handleCardSuffle}>
+                   Card Shuffle
+                   </button>
+                   )
+                }
+
+                
+                {/* <div className='divider'></div> */}
                 <div className='shuffle-area'>
-                    <div className='card one'>Card 1</div>
-                    <div className='card two'>Card 2</div>
-                    <div className='card three'>Card 3</div>
-                    <div className='card four'>Card 4</div>
+                    <div className='card one' ref={cardRefs[0]} onClick={()=>handleCardClick(0)}>Card 1</div>
+                    <div className='card two' ref={cardRefs[1]} onClick={()=>handleCardClick(1)}>Card 2</div>
+                    <div className='card three' ref={cardRefs[2]} onClick={()=>handleCardClick(2)}>Card 3</div>
+                    <div className='card four' ref={cardRefs[3]} onClick={()=>handleCardClick(3)}>Card 4</div>
                 </div>
             </div>
             {/* Chat area with conditional style */}
