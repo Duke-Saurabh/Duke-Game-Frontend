d4657cc35192e12fcf9af47b841f27c693fab0cf
Duke-Saurabh <ds8123495@example.com>
2024-07-07 21:24:37 +0530

    ss

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,4 +1,8 @@
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:3000');
+// const socket = io('http://localhost:3000');
+// const socket = io('https://duke-game-backend.vercel.app/api/');
+const socket = io('/socket.io/',{
+    ws: true,
+});
 export default socket;
