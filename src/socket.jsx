2eca979b2595730066e3e0f78149ccd0c7a3793d
Duke-Saurabh <97291360+Duke-Saurabh@users.noreply.github.com>
2024-08-22 00:28:02 +0530

    Update socket.jsx

--- src/socket.jsx
+++ src/socket.jsx
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
 // const socket = io('http://localhost:3000');
-const socket = io('https://duke-game-backend.vercel.app/api/');
+const socket = io('https://duke-game-backend-2.onrender.com/');
 
 export default socket;
